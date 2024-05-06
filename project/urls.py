ea95445ad2592b09eed2cc621bb89ac820fbe038
Lucas Malta <flucasofficial@gmail.com>
2024-05-06 11:34:14 -0300

    startapp files recipes

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('HOME')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+def contato(request):
+    return HttpResponse('CONTATO')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), # HOME
+    path('sobre/', sobre), # /sobre/
+    path('contato/', contato), # /contato/
 ]
